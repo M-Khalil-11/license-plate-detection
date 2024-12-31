6b848b23e04e39e8592466f1223754192c2c1f0c
M-Khalil-11 <mcrizamm@gmail.com>
2024-12-31 16:50:57 +0100

    Update config.py

--- src/utils/config.py
+++ src/utils/config.py
@@ -4,10 +4,6 @@ class Config:
     PLATE_MODEL_PATH = "src/models/plate_model.pt"
     OCR_MODEL_PATH = "src/models/ocr_model.pt"
 
-    # ROI settings
-    ROI_HEIGHT_1 = 200
-    ROI_HEIGHT_2 = 300
-
     # Output settings
     INPUT_VIDEO_PATH = "data/input/tc.mp4"
     OUTPUT_VIDEO_PATH = "data/output/tc.avi"
